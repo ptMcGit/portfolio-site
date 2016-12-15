5131adc0c798bd9aab4c110b2b4926ea926e318d
Zach H <zdhadgraft@gtcc.edu>
2016-12-15 16:41:35 -0500

    Converted sites section to slideshow.

--- content/js/main.js
+++ content/js/main.js
@@ -2,4 +2,5 @@ window.onload = function() {
     $.getScript('./js/modal.js');
     $.getScript('./js/slideshow.js');
     $.getScript('./js/video_slideshow.js');
+    $.getScript('./js/sites_slideshow.js');
 };
---
+++ content/js/sites_slideshow.js
@@ -0,0 +1,107 @@
+(function() {
+
+    // Sites slideshow
+
+    /*jshint multistr: true */
+
+    var n_rooms = {
+        site_url:       "https://reddit-clone456.herokuapp.com/",
+        img_url:        "../img/nrooms_ss.png",
+        alt_text:       "N Rooms Screenshot",
+        code_url:       "https://github.com/ptMcGit/reddit_clone",
+        short_desc:     "A social media platform with reddit-like features.",
+        tech_desc:      "(Ruby on Rails, heroku).",
+        work_desc:      "\u2022 front-end development: Bootstrap, jQuery<br>\
+\u2022 back-end development: PostgreSQL, devise, pundit, RSpec, Capybara",
+        background:     "#4f97ce"
+    };
+
+    var pearl = {
+        site_url:       "https://bubble-a6b08.firebaseapp.com/#/home",
+        img_url:        "../img/pearl_ss.png",
+        alt_text:       "Pearl Screenshot",
+        code_url:       "https://github.com/TIY-LiveLocal/Pearl",
+        short_desc:     "A web application that connects users with local businesses.",
+        tech_desc:      "(Ruby on Rails, heroku, Firebase)",
+        work_desc:      "\u2022 basic CRUD and RSpec testing<br>\
+\u2022 Sidekiq workers with Redis for RESTful calls to Locu, Yelp, and Google Places APIs<br>\
+\u2022 Basic authentication solution",
+        background:     "#6dc0d1"
+    };
+
+    var portfolio = {
+        site_url:       "https://ptmcgit.github.io",
+        img_url:        "../img/this_site_ss.png",
+        alt_text:       "Portfolio Site Screenshot",
+        code_url:       "https://github.com/ptMcGit/portfolio-site",
+        short_desc:     "The code for this site.",
+        tech_desc:      "(nanoc)",
+        work_desc:      "\u2022 jQuery, Font-Awesome, SCSS",
+        background:     "#48423f"
+    };
+
+    /*jshint multistr: false */
+
+    var sites = [
+        n_rooms,
+        pearl,
+        portfolio
+    ];
+
+    var siteCollection = new SlideShow(sites);
+
+    siteCollection.bindElement(function(that) {
+        document.getElementById("site-slideshow-site_url")
+            .href = that.site_url;
+    });
+
+    siteCollection.bindElement(function(that) {
+        var i = document.getElementById("site-slideshow-img_url");
+        i.src = that.img_url;
+        i.alt = that.alt_text;
+    });
+
+    siteCollection.bindElement(function(that) {
+        document.getElementById("site-slideshow-code_url")
+            .href = that.code_url;
+        //.setAttribute("href", that.code_url);
+    });
+
+    siteCollection.bindElement(function(that) {
+        document.getElementById("site-slideshow-short_desc")
+            .innerHTML = that.short_desc;
+    });
+
+    siteCollection.bindElement(function(that) {
+        document.getElementById("site-slideshow-tech_desc")
+            .innerHTML = that.tech_desc;
+    });
+
+    siteCollection.bindElement(function(that) {
+        document.getElementById("site-slideshow-work_desc")
+            .innerHTML = that.work_desc;
+    });
+
+    siteCollection.bindElement(function(that) {
+        document.getElementById("sites")
+            .style.backgroundColor = that.background;
+    });
+
+    // associate html elements with events
+
+    // prev slide
+    siteCollection.bindPrevEvent(
+        document.getElementById("site-slideshow-prev-button"),
+        "click"
+    );
+
+    // next slide
+    siteCollection.bindNextEvent(
+        document.getElementById("site-slideshow-next-button"),
+        "click"
+    );
+
+    // set the first slide
+    siteCollection.setSlide(0);
+
+})();
