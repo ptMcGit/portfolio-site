ee44c107537902d08f34ebd4e1da44ed07bacd1b
Zach H <zdhadgraft@gtcc.edu>
2017-01-06 17:12:30 -0500

    Added webpack.

---
+++ content/js/entry.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    require('./modal.js');
+    require('./slideshow.js');
+    require('./video_slideshow.js');
+    require('./sites_slideshow.js');
+    require('./photo_slideshow.js');
+};
--- content/js/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    $.getScript('./js/modal.js');
-    $.getScript('./js/slideshow.js');
-    $.getScript('./js/video_slideshow.js');
-    $.getScript('./js/sites_slideshow.js');
-    $.getScript('./js/photo_slideshow.js');
-};
