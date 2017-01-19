4e8b426df3c8d35288816178e4f95b71f4386dba
Zach H <zdhadgraft@gtcc.edu>
2017-01-19 13:06:50 -0500

    Added email modal.

---
+++ content/js/email_modal.js
@@ -0,0 +1,8 @@
+// About me modal
+
+(function() {
+    var EasyModal = require('easymodal');
+
+    var emailModal = new EasyModal('#email-me','#email-button');
+
+})();
--- content/js/entry.js
+++ content/js/entry.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    require('./email_modal');
     require('./aboutme_modal');
     require('./video_modal');
     require('./photo_modal');
