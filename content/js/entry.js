83b923536027fc3bcf1c7ed31dd30a1665657067
Zach H <zdhadgraft@gtcc.edu>
2017-01-18 22:21:42 -0500

    Added video modal.

--- content/js/entry.js
+++ content/js/entry.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     require('./aboutme_modal');
+    require('./video_modal');
     require('./video_slideshow.js');
     require('./sites_slideshow.js');
     require('./photo_slideshow.js');
---
+++ content/js/video_modal.js
@@ -0,0 +1,8 @@
+// Video modal
+
+(function(){
+    var EasyModal = require('easymodal');
+
+    var videoModal = new EasyModal('#video', '#video-button');
+
+})();
