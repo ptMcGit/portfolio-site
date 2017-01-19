93ab0bd8f3874f32f43026e7c26dcb0e6927b01c
Zach H <zdhadgraft@gtcc.edu>
2017-01-19 12:52:03 -0500

    Added/removed photo slides.

--- content/js/photo_slides.js
+++ content/js/photo_slides.js
@@ -1,6 +1,6 @@
 module.exports = [
+    {url: "https://c1.staticflickr.com/1/437/31595863663_7a16c14d4d_o.jpg"},
     {url: "https://c3.staticflickr.com/6/5568/30553080122_3b008191eb.jpg"},
-    {url: "https://c1.staticflickr.com/9/8707/30110056232_5185f3a791.jpg"},
     {url: "https://c7.staticflickr.com/6/5832/29402607974_4e640f8416.jpg"},
     {url: "https://c7.staticflickr.com/9/8426/29772722582_4dcec4e735.jpg"},
     {url: "https://c5.staticflickr.com/9/8644/29712946812_13a3ba2be1.jpg"},
@@ -23,7 +23,6 @@ module.exports = [
     {url: "https://c8.staticflickr.com/9/8515/28775030703_6fd6ed09ae.jpg"},
     {url: "https://c2.staticflickr.com/9/8360/28775031353_4c4c259b32.jpg"},
     {url: "https://c4.staticflickr.com/9/8510/28775031483_1a18fb55d7.jpg"},
-    {url: "https://c5.staticflickr.com/1/373/31397299612_80b2d3338c.jpg"},
     {url: "https://c1.staticflickr.com/1/481/32103842931_e0fcf06fc4_o.jpg"},
     {url: "https://c1.staticflickr.com/1/477/31983452200_a33b5116ee_o.jpg"}
 ];
