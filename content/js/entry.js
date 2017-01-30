90574c25e101e3e049b1b6d59fcede7918a7243d
Zach H <zdhadgraft@gtcc.edu>
2017-01-30 12:36:56 -0500

    Reorganized JS folder.

--- content/js/entry.js
+++ content/js/entry.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-    require('./top_link.js');
-    require('./email_modal');
-    require('./aboutme_modal');
-    require('./video_modal');
-    require('./photo_modal');
-    require('./video_slideshow.js');
-    require('./sites_slideshow.js');
-    require('./photo_slideshow.js');
+    require('./top_link/top_link.js');
+    require('./email_me/email_modal');
+    require('./about_me/aboutme_modal');
+    require('./sites_slideshow/sites_slideshow.js');
+    require('./video_modal/video_modal');
+    require('./video_modal/video_slideshow.js');
+    require('./photo_modal/photo_modal');
+    require('./photo_modal/photo_slideshow.js');
 };
